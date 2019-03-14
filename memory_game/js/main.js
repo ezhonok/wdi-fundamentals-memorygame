3f0d5e8ab3d9e9090c7739531f4839ddf08498c8
Svetlana Polyakova <juchini@Svetlanas-MacBook-Air.local>
2019-03-13 20:13:24 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,17 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[1];
-cardsInPlay.push('cardOne');
-cardOne;
-console.log("User flipped queen");
-var cardTwo = cards[3];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again");
+		console.log("Sorry, try again");
 	}
 };
+
+var flipCard = function(cardId) {
+console.log("User flipped" + " " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+};
+flipCard(0);
+flipCard(2);
+flipCard(5);
